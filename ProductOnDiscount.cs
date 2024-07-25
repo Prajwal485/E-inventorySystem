326398e2ade4455cd98a9eaeef653e34c04b8220
V Hiremath <pvhiremath@deloitte.com>
2024-07-25 14:46:23 +0530

    ProductOnDiscount.cs committed By Anand

---
+++ ProductOnDiscount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handson_1
+{
+    internal class ProductOnDiscount
+    {
+        public double DiscountPercentage { get; set; }
+
+        public int MinimumPickQuantity { get; set; }
+
+
+        
+    }
+}
