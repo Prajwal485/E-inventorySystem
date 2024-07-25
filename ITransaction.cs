0eafad2e6fdf07c53f1d76153bfbed6e0975c93d
V Hiremath <pvhiremath@deloitte.com>
2024-07-25 14:52:19 +0530

    ITransaction committed

---
+++ ITransaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handson_2
+{
+    internal interface ITransaction
+    {
+        long  FromAccount { get; }
+
+        long ToAccount { get; }
+
+        void TransferAccount(long fromAccount, long toAccount);
+
+
+
+
+
+
+    }
+}
